fbe3bb29cbd50c4b73ffba1c38a631cbc940edaa
Charlín <Charlincano12@gmail.com>
2022-11-14 17:43:39 -0500

    actualizando ramaC

---
+++ js/LOGO.js
@@ -0,0 +1,87 @@
+const btnDepartamentos = document.getElementById('btn-LOGO1'),
+	  btnCerrarMenu = document.getElementById('btn-menu-cerrar'),
+	  grid = document.getElementById('gridL1'),
+	  contenedorEnlacesNav = document.querySelector('#menuL1 .contenedor-enlaces-nav1'),
+	  contenedorSubCategorias = document.querySelector('#gridL1 .contenedor-subcategorias'),
+	  esDispositivoMovil = () => window.innerWidth <= 800;
+
+btnDepartamentos.addEventListener('mouseover', () => {
+	if(!esDispositivoMovil()){
+		grid.classList.add('activo');
+	}
+});
+
+grid.addEventListener('mouseleave', () => {
+	if(!esDispositivoMovil()){
+		grid.classList.remove('activo');
+	}
+});
+
+document.querySelectorAll('#menuL1 .categorias a').forEach((elemento) => {
+	elemento.addEventListener('mouseenter', (e) => {
+		if(!esDispositivoMovil()){
+			document.querySelectorAll('#menuL1 .subcategoria').forEach((categoria) => {
+				categoria.classList.remove('activo');
+				if(categoria.dataset.categoria == e.target.dataset.categoria){
+					categoria.classList.add('activo');
+				}
+			});
+		};
+	});
+});
+
+// EventListeners para dispositivo movil.
+document.querySelector('#btn-menu-barras').addEventListener('click', (e) => {
+	e.preventDefault();
+	if(contenedorEnlacesNav.classList.contains('activo')){
+		contenedorEnlacesNav.classList.remove('activo');
+		document.querySelector('body').style.overflow = 'visible';
+	} else {
+		contenedorEnlacesNav.classList.add('activo');
+		document.querySelector('body').style.overflow = 'hidden';
+	}
+});
+
+// Click en boton de todos los elementos (Para version movil).
+btnDepartamentos.addEventListener('click', (e) => {
+	e.preventDefault();
+	grid.classList.add('activo');
+	btnCerrarMenu.classList.add('activo');
+});
+
+// Boton de regresar en el menu de categorias
+document.querySelector('#gridL1 .categorias .btn-regresar').addEventListener('click', (e) => {
+	e.preventDefault();
+	grid.classList.remove('activo');
+	btnCerrarMenu.classList.remove('activo');
+});
+
+document.querySelectorAll('#menuL1 .categorias a').forEach((elemento) => {
+	elemento.addEventListener('click', (e) => {
+		if(esDispositivoMovil()){
+			contenedorSubCategorias.classList.add('activo');
+			document.querySelectorAll('#menuL1 .subcategoria').forEach((categoria) => {
+				categoria.classList.remove('activo');
+				if(categoria.dataset.categoria == e.target.dataset.categoria){
+					categoria.classList.add('activo');
+				}
+			});
+		}
+	});
+});
+
+// Boton de regresar en el menu de categorias
+document.querySelectorAll('#gridL1 .contenedor-subcategorias .btn-regresar').forEach((boton) => {
+	boton.addEventListener('click', (e) => {
+		e.preventDefault();
+		contenedorSubCategorias.classList.remove('activo');
+	});
+});
+
+btnCerrarMenu.addEventListener('click', (e)=> {
+	e.preventDefault();
+	document.querySelectorAll('#menuL1 .activo').forEach((elemento) => {
+		elemento.classList.remove('activo');
+	});
+	document.querySelector('body').style.overflow = 'visible';
+});
